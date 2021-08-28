51fec17471a18a42fa1166a388daa05bb535bb26
wilsonplau <hello@wilsonplau.com>
2021-08-28 10:38:00 +0900

    chore: reformat BinarySearch and FirstBadVersion

--- src/Arrays/BinarySearch/BinarySearch.java
+++ src/Arrays/BinarySearch/BinarySearch.java
@@ -4,7 +4,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 /**
- * Binary Search | Leetcode #704 | Easy
+ * Binary Search | Leetcode #0704 | Easy
  * https://leetcode.com/problems/binary-search/
  * 
  * Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. 
@@ -13,23 +13,23 @@ import static org.junit.Assert.*;
  */
 
 /**
- * Solution Notes by _______ 
+ * Solution Notes by _______
  *
  */
 
 public class BinarySearch {
 
-    public static int search(int nums[], int target) {
-        // TODO: Your solution
-        return -1;
-    }
- 
-    @Test
-    public static void main(String[] args) {
-        int[] nums1 = {-1,0,3,5,9,12};
-        assertEquals(4, search(nums1, 9));
-    
-        int[] nums2 = {-1,0,3,5,9,12};
-        assertEquals(-1, search(nums2, 2));
-    }
+	public static int search(int nums[], int target) {
+		// TODO: Your solution
+		return -1;
+	}
+
+	@Test
+	public static void main(String[] args) {
+		int[] nums1 = { -1, 0, 3, 5, 9, 12 };
+		assertEquals(4, search(nums1, 9));
+
+		int[] nums2 = { -1, 0, 3, 5, 9, 12 };
+		assertEquals(-1, search(nums2, 2));
+	}
 }
